a240233de068f50f1eb23756c6b4b066e38bfa55
Paulo Vitor <pv.lima02@gmail.com>
2022-02-18 13:27:26 -0300

    feat: ensure RemoteAuthentication returns an Account on success

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,4 +1,7 @@
-import { AuthenticationParams } from '@/domain/usecases/authentication';
+import {
+  Authentication,
+  AuthenticationParams,
+} from '@/domain/usecases/authentication';
 import { HttpPostClient } from '@/data/protocols/http/http-post-client';
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error';
 import { HttpStatusCode } from '@/data/protocols/http/http-response';
@@ -6,7 +9,7 @@ import { UnexpectedError } from '@/domain/errors/unexpected-error';
 import { NotFoundError } from '@/domain/errors/not-found-error';
 import { AccountModel } from '@/domain/models/account-model';
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor(
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<
@@ -15,13 +18,14 @@ export class RemoteAuthentication {
     >,
   ) {}
 
-  async auth(params: AuthenticationParams): Promise<void> {
+  async auth(params: AuthenticationParams): Promise<AccountModel> {
     const response = await this.httpPostClient.post({
       url: this.url,
       body: params,
     });
     switch (response.statusCode) {
       case HttpStatusCode.ok:
+        return response.body;
         break;
       case HttpStatusCode.unauthorized:
         throw new InvalidCredentialsError();
