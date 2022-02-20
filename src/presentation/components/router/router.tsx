bfe26e467be6aa1c262d0c1b8c74fc66236ae36e
Paulo Vitor <pv.lima02@gmail.com>
2022-02-20 10:15:16 -0300

    fix: import global styles in main/index.tsx

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Login } from '@/presentation/pages';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import '@/presentation/styles/global.scss';
 
 export function Router() {
   return (
