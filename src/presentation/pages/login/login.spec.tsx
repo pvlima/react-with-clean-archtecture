fc258ea582b3789e6f58227a5d5d599b8edba9f5
Paulo Vitor <pv.lima02@gmail.com>
2022-02-20 10:56:53 -0300

    test: ensure Login starts with form initial state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -9,5 +9,11 @@ describe('Login Component', () => {
     expect(errorWrap.childElementCount).toBe(0);
     const submitButton = getByTestId('submit') as HTMLButtonElement;
     expect(submitButton.disabled).toBeTruthy();
+    const emailStatus = getByTestId('email-status') as HTMLInputElement;
+    expect(emailStatus.title).toBe('Campo obrigatório');
+    expect(emailStatus.textContent).toBe('🔴');
+    const passwordStatus = getByTestId('password-status') as HTMLInputElement;
+    expect(passwordStatus.title).toBe('Campo obrigatório');
+    expect(passwordStatus.textContent).toBe('🔴');
   });
 });
