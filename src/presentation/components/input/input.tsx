13fbd5fa2b6d9545bac14fa41344dbc51ebf9f3b
Paulo Vitor <pv.lima02@gmail.com>
2022-02-20 09:25:54 -0300

    feat: remove browser input autofill

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -6,9 +6,20 @@ type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
 };
 
 export function Input({ name, ...props }: InputProps) {
+  function enableInput(e: React.FocusEvent<HTMLInputElement>) {
+    e.target.readOnly = false;
+  }
+
   return (
     <div className={Styles.inputWrap}>
-      <input id={name} name={name} {...props} />
+      <input
+        id={name}
+        name={name}
+        {...props}
+        readOnly
+        onFocus={enableInput}
+        autoComplete="off"
+      />
       <label htmlFor={name} className={Styles.status}>
         🔴
       </label>
