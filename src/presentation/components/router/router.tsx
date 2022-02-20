ca95c48b328ed16a4ef103972c9d13e2f3ebf2bb
Paulo Vitor <pv.lima02@gmail.com>
2022-02-20 09:18:10 -0300

    refactor: move global styles to a separate file

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Login } from '@/presentation/pages';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import '@/presentation/styles/global.scss';
 
 export function Router() {
   return (
