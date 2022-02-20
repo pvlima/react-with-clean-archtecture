9e72c53ce18e0f53f2f7a48dadda57d7ec1b3afe
Paulo Vitor <pv.lima02@gmail.com>
2022-02-20 12:41:22 -0300

    refactor: change validator interface

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -24,11 +24,11 @@ export function Login({ validation }: LoginProps) {
   });
 
   useEffect(() => {
-    validation.validate({ email: state.email });
+    validation.validate('email', state.email);
   }, [validation, state.email]);
 
   useEffect(() => {
-    validation.validate({ password: state.password });
+    validation.validate('password', state.password);
   }, [validation, state.password]);
 
   return (
