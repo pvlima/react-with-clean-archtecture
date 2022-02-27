8192fdf1bbbe1f738530230de81bad5181d46f16
Paulo Vitor <pv.lima02@gmail.com>
2022-02-27 19:02:42 -0300

    fix: ensure EmailValidation allows empty value

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -10,13 +10,19 @@ function makeSut() {
 describe('EmailValidation', () => {
   it('shold return error if email is invalid', () => {
     const { sut } = makeSut();
-    const error = sut.validate('');
+    const error = sut.validate(faker.random.word());
     expect(error).toEqual(new InvalidFieldError());
   });
 
   it('shold return falsy if email is valid', () => {
     const { sut } = makeSut();
     const error = sut.validate(faker.internet.email());
+    expect(error).toBeFalsy();
+  });
+
+  it('shold return falsy if email is empty', () => {
+    const { sut } = makeSut();
+    const error = sut.validate('');
     expect(error).toBeFalsy();
   });
 });
