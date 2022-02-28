825b618a5f69100823090c53ead45889497efee2
Paulo Vitor <pv.lima02@gmail.com>
2022-02-28 19:55:30 -0300

    test: ensure ValidationBuilder has a email helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,12 +1,22 @@
 import faker from 'faker';
-import { RequiredFieldValidation } from '@/validation/validators';
+import {
+  RequiredFieldValidation,
+  EmailValidation,
+} from '@/validation/validators';
 import { ValidationBuilder as sut } from './validation-builder';
 
 describe('ValidationBuilder', () => {
-  it('should return RequiredFieldValidator', () => {
+  it('should return RequiredFieldValidation', () => {
     const fieldName = faker.database.column();
     const validations = sut.field(fieldName).required().build();
     expect(validations.length).toBe(1);
     expect(validations).toEqual([new RequiredFieldValidation(fieldName)]);
+  });
+
+  it('should return EmailFieldValidation', () => {
+    const fieldName = faker.database.column();
+    const validations = sut.field(fieldName).email().build();
+    expect(validations.length).toBe(1);
+    expect(validations).toEqual([new EmailValidation(fieldName)]);
   });
 });
