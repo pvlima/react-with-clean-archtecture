e97fc65a0f3a0c5562ea2d85d11258a41d9edf00
Paulo Vitor <pv.lima02@gmail.com>
2022-02-28 21:56:04 -0300

    test: ensure Input Component remove readOnly on focus

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,22 +1,33 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { FormContextProvider } from '@/presentation/contexts';
 import { Input } from '..';
 
+function makeSut() {
+  return render(
+    <FormContextProvider
+      value={{
+        state: { isLoading: false, email: '', password: '' },
+        setState: () => {},
+      }}
+    >
+      <Input name="field" />
+    </FormContextProvider>,
+  );
+}
+
 describe('Input Component', () => {
   it('should begin with readOnly', () => {
-    const { getByTestId } = render(
-      <FormContextProvider
-        value={{
-          state: { isLoading: false, email: '', password: '' },
-          setState: () => {},
-        }}
-      >
-        <Input name="field" />
-      </FormContextProvider>,
-    );
-    const input = getByTestId('field') as HTMLInputElement;
+    const sut = makeSut();
+    const input = sut.getByTestId('field') as HTMLInputElement;
     expect(input.readOnly).toBe(true);
+  });
+
+  it('should remove readOnly on focus', () => {
+    const sut = makeSut();
+    const input = sut.getByTestId('field') as HTMLInputElement;
+    fireEvent.focus(input);
+    expect(input.readOnly).toBe(false);
   });
 });
